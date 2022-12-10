cd59c782e4b861becad57fa45a7c0a280251bd6b
Okazaki Kohei <marie673g@gmail.com>
2022-12-10 19:04:06 +0900

    add: request piece

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import src.global_value as gv
 import src.application.bittorrent as b
 from src.domain.entity.torrent import Torrent
 
 
 def main():
-    path = "/root/evaluation/torrent_file/128MB.torrent"
+    paths = [
+        "128MB.torrent",
+        "256MB.torrent",
+        "512MB.torrent",
+        "1024MB.torrent",
+        "2048MB.torrent",
+    ]
+    gv.EVALUATION_PATH = gv.EVALUATION_PATH + paths[int(sys.argv[1])]
+    path = gv.TORRENT_FILE_PATH + paths[int(sys.argv[1])]
+
     torrent = Torrent(path)
     bp = b.BitTorrent(torrent)
     bp.start()
-    bp.join()
 
 
 if __name__ == "__main__":
