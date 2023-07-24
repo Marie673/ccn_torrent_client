6798792957a085e96589987ee313d54b458ff4fe
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-07-24 12:54:03 +0900

    change

--- src/global_value.py
+++ src/global_value.py
@@ -1,5 +1,7 @@
+import os
+
 EVALUATION_PATH = "/client/evaluation/ccn_client/"
-TORRENT_FILE_PATH = "/client/evaluation/torrent_file/"
+TORRENT_FILE_PATH = os.environ["HOME"] + "/torrent/"
 CACHE_PATH = "/client/proxy_cache/"
 
 CHUNK_SIZE = 1024 * 4
