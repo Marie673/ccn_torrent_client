e681f57bd1cc392b305b7c7add3cf428685fff0a
Okazaki <net.kohei.okazaki@gmail.com>
2023-07-27 07:21:11 +0900

    change

--- src/application/bittorrent.py
+++ src/application/bittorrent.py
@@ -128,6 +128,7 @@ class BitTorrent:
                             name=self.name,
                             chunk_num=chunk_num
                         )
+                        piece.blocks[block_index].state = State.PENDING
                         piece.blocks[block_index].last_seen = time.time()
                         self.cubic.now_wind += 1
                         # logger.debug(f"Send interest: {piece_index}, {chunk_num}")
