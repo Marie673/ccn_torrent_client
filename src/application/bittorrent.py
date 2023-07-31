8301409e0dc3d18b5aed3f161eb08dd49487e377
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-07-31 12:11:01 +0900

    change

--- src/application/bittorrent.py
+++ src/application/bittorrent.py
@@ -141,7 +141,7 @@ class BitTorrent:
                 block_index = chunk_num % self.chunks_per_piece
 
                 self.cubic.cals_cwind()
-                if not self.cubic.now_wind < 10000:
+                if not self.cubic.now_wind < 25600:
                     break
 
                 if piece.blocks[block_index].state == State.FREE:
