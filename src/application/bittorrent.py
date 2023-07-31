55cc307b58a0cbaceabadb12f820d766487f34ca
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-07-31 09:18:16 +0900

    change

--- src/application/bittorrent.py
+++ src/application/bittorrent.py
@@ -107,7 +107,7 @@ class BitTorrent:
         try:
             while not self.all_pieces_completed():
                 info = self.cef_handle.receive(timeout_ms=4000)
-                # logger.debug(info)
+                logger.debug(info.name, info.chunk_num)
                 if info.is_succeeded and info.is_data:
                     prefix = info.name.split('/')
                     if prefix[0] != 'ccnx:':
