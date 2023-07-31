c06424a239fb0b0bc9383bb6c9f1d21b48064395
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-07-31 16:27:21 +0900

    change

--- src/application/bittorrent.py
+++ src/application/bittorrent.py
@@ -1,10 +1,14 @@
 import asyncio
 import concurrent.futures
 import os
+import threading
 
 import cefpyco
 import time
 import bitstring
+import select
+import socket
+import errno
 
 from src.application.cubic import Cubic
 from src.domain.entity.piece.piece import Piece
@@ -15,19 +19,18 @@ import multiprocessing
 from threading import Thread
 import datetime
 
-
 from logger import logger
 
 CHUNK_SIZE = 1024 * 4
-CACHE_PATH = os.environ['HOME']+"/proxy_cache/"
+CACHE_PATH = os.environ['HOME'] + "/proxy_cache/"
 MAX_PEER_CONNECT = 1
 TIME_OUT = 4
 EVALUATION = True
 EVALUATION_PATH = "/client/evaluation/ccn_client/test"
 
 
-class BitTorrent:
-    def __init__(self, torrent: Torrent):
+class BitTorrent :
+    def __init__(self, torrent: Torrent) :
         """
         トレントファイル解析
         ↓
@@ -42,25 +45,25 @@ class BitTorrent:
 
         self.name = "ccnx:/BitTorrent/" + self.info_hash
 
-        try:
+        try :
             os.makedirs(self.file_path)
-        except Exception as e:
+        except Exception as e :
             logger.error(e)
 
         # number_of_pieces の計算
-        if torrent.file_mode == FileMode.single_file:
+        if torrent.file_mode == FileMode.single_file :
             self.number_of_pieces = int(self.info.length / self.info.piece_length)
-        else:
+        else :
             length: int = 0
-            for file in self.info.files:
+            for file in self.info.files :
                 length += file.length
             self.number_of_pieces = int(length / self.info.piece_length)
 
         # 1ピース当たりのチャンク数
         # ピースの最後を表現するときに、チャンクサイズで余りが出ても次のピースデータを含めない.
-        if self.info.piece_length % CHUNK_SIZE == 0:
+        if self.info.piece_length % CHUNK_SIZE == 0 :
             self.chunks_per_piece = self.info.piece_length // CHUNK_SIZE
-        else:
+        else :
             self.chunks_per_piece = (self.info.piece_length // CHUNK_SIZE) + 1
 
         # end_chunk_numの計算.
@@ -80,27 +83,55 @@ class BitTorrent:
         self.started_time = 0
 
     def run(self):
-        queue = multiprocessing.Queue()
-
         try:
-            process = multiprocessing.Process(target=self.cef_listener, args=(queue,))
-            process.start()
+            t = threading.Thread(target=self.cef_listener())
+            t.start()
         except Exception as e:
             logger.error(e)
             raise e
 
         self.started_time = time.time()
         try:
-            self.request_piece_handle(queue)
+            self.request_piece_handle()
         except Exception as e:
             logger.error(e)
             raise e
         except KeyboardInterrupt:
             return
         finally:
-            process.kill()
-            process.join()
+            t.join()
+            pass
 
+    def cef_listener(self):
+        while not self.all_pieces_completed():
+            read = self.cef_handle.sock
+            read_list, _, _ = select.select(read, [], [], 60)
+
+            def read_from_socket(sock):
+                data = b''
+
+                while True:
+                    try:
+                        buff = sock.recv(4096)
+                        if len(buff) <= 0:
+                            break
+
+                        data += buff
+                    except socket.error as e:
+                        err = e.args[0]
+                        if err != errno.EAGAIN or err != errno.EWOULDBLOCK:
+                            logger.debug("Wrong errno {}".format(err))
+                        break
+                    except Exception as e:
+                        logger.exception(f"Recv failed. {e}")
+                        break
+
+                return data
+
+            data = read_from_socket(read)
+            logger.debug(data)
+
+    """
     def cef_listener(self, queue):
         logger.debug("start cef listener")
         try:
@@ -126,8 +157,9 @@ class BitTorrent:
             logger.error(e)
         except KeyboardInterrupt:
             return
+    """
 
-    def request_piece_handle(self, queue: multiprocessing.Queue):
+    def request_piece_handle(self):
         logger.debug("requester is start")
         last_time = time.time()
 
@@ -136,14 +168,6 @@ class BitTorrent:
 
             # ここでqueueの状態を監視しないとサイズが膨れ上がる
             for chunk_num in range(self.end_chunk_num + 1):
-                while queue.qsize() > 0:
-                    logger.debug(f"start get {time.time()}")
-                    info = queue.get()
-                    logger.debug(f"end get {time.time()}")
-                    logger.debug(f"{time.time()} {info.name} {info.chunk_num}")
-                    # logger.debug(f"{queue.qsize()}")
-                    self.handle_piece(info)
-                logger.debug("end observer")
 
                 piece_index = chunk_num // self.chunks_per_piece
                 piece = self.pieces[piece_index]
@@ -153,7 +177,7 @@ class BitTorrent:
                 if not self.cubic.now_wind < 5000:
                     break
 
-                if piece.blocks[block_index].state == State.FREE:
+                if piece.blocks[block_index].state == State.FREE :
                     self.cef_handle.send_interest(
                         name=self.name,
                         chunk_num=chunk_num
@@ -163,47 +187,47 @@ class BitTorrent:
                     self.cubic.now_wind += 1
                     # logger.debug(f"Send interest: {piece_index}, {chunk_num}")
 
-            if time.time() - last_time > 1:
+            if time.time() - last_time > 1 :
                 logger.debug(f"c_window: {int(self.cubic.cwind)} qsize: {queue.qsize()}")
                 self.print_progress()
                 last_time = time.time()
 
-    def check_chunk_state(self):
+    def check_chunk_state(self) :
         pending_chunk_num = 0
-        for chunk_num in range(self.end_chunk_num + 1):
+        for chunk_num in range(self.end_chunk_num + 1) :
             piece_index = chunk_num // self.chunks_per_piece
             piece = self.pieces[piece_index]
             block_index = chunk_num % self.chunks_per_piece
 
-            if piece.blocks[block_index].state == State.PENDING:
-                if time.time() - piece.blocks[block_index].last_seen > TIME_OUT:
+            if piece.blocks[block_index].state == State.PENDING :
+                if time.time() - piece.blocks[block_index].last_seen > TIME_OUT :
                     piece.blocks[block_index].state = State.FREE
                     piece.blocks[block_index].last_seen = time.time()
                     self.cubic.last_time_loss = time.time()
                     self.cubic.w_max = self.cubic.cwind
-                else:
+                else :
                     pending_chunk_num += 1
 
         self.cubic.now_wind = pending_chunk_num
 
-    def handle_piece(self, info):
+    def handle_piece(self, info) :
         payload = info.payload
         chunk_num = info.chunk_num
 
         piece_index = chunk_num // self.chunks_per_piece
         offset = (chunk_num % self.chunks_per_piece) * CHUNK_SIZE
         piece = self.pieces[piece_index]
-        if piece.is_full:
+        if piece.is_full :
             return
 
         piece.set_block(offset=offset, data=payload)
-        if piece.are_all_blocks_full():
-            if piece.set_to_full():
+        if piece.are_all_blocks_full() :
+            if piece.set_to_full() :
                 self.bitfield[piece_index] = 1
                 self.complete_pieces += 1
                 piece.write_on_disk()
 
-    def _generate_pieces(self) -> List[Piece]:
+    def _generate_pieces(self) -> List[Piece] :
         """
         torrentの全てのpieceを生成して初期化
         :return: List[Piece]
@@ -211,29 +235,29 @@ class BitTorrent:
         pieces: List[Piece] = []
         last_piece = self.number_of_pieces - 1
 
-        for i in range(self.number_of_pieces):
+        for i in range(self.number_of_pieces) :
             start = i * 20
             end = start + 20
 
-            if i == last_piece:
+            if i == last_piece :
                 piece_length = self.info.length - (self.number_of_pieces - 1) * self.info.piece_length
-                pieces.append(Piece(i, piece_length, self.info.pieces[start:end], self.file_path))
-            else:
-                pieces.append(Piece(i, self.info.piece_length, self.info.pieces[start:end], self.file_path))
+                pieces.append(Piece(i, piece_length, self.info.pieces[start :end], self.file_path))
+            else :
+                pieces.append(Piece(i, self.info.piece_length, self.info.pieces[start :end], self.file_path))
 
         return pieces
 
-    def all_pieces_completed(self) -> bool:
-        for piece in self.pieces:
-            if not piece.is_full:
+    def all_pieces_completed(self) -> bool :
+        for piece in self.pieces :
+            if not piece.is_full :
                 return False
         return True
 
-    def print_progress(self):
+    def print_progress(self) :
         block_num = 0
-        for piece in self.pieces:
-            for block in piece.blocks:
-                if block.state == State.FULL:
+        for piece in self.pieces :
+            for block in piece.blocks :
+                if block.state == State.FULL :
                     block_num += 1
 
         progress = (block_num / (self.end_chunk_num + 1)) * 100
