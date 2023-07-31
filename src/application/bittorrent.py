863e51b8485e1b28afbbbaeb55c339886b12481c
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-07-31 16:39:43 +0900

    change

--- src/application/bittorrent.py
+++ src/application/bittorrent.py
@@ -104,6 +104,7 @@ class BitTorrent :
 
     def cef_listener(self):
         while not self.all_pieces_completed():
+            logger.debug(f"{self.cef_handle.__dict__}")
             read = self.cef_handle.handler
             logger.debug(read)
             read_list, _, _ = select.select(read, [], [], 60)
